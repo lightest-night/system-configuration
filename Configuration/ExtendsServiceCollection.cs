bed668672ca93a9ef081a6a6b33ccd7e45d62f34
James Woodley <james@lightestnight.com>
2022-01-05 14:14:43 +0000

    Use TryAddSingleton

--- Configuration/ExtendsServiceCollection.cs
+++ Configuration/ExtendsServiceCollection.cs
@@ -1,17 +1,22 @@
 ﻿using System;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 
 namespace LightestNight.Configuration
 {
     public static class ExtendsServiceCollection
     {
         public static IServiceCollection AddLightestNightConfiguration(this IServiceCollection services,
-            IConfigurationBuilder? builder = null, string[]? args = null, Func<ConfigurationBuilder, ConfigurationBuilder>? customizer = null)
-            => services.AddSingleton<IConfigurationManager>(serviceProvider =>
+                IConfigurationBuilder? builder = null, string[]? args = null,
+                Func<ConfigurationBuilder, ConfigurationBuilder>? customizer = null)
+        {
+            services.TryAddSingleton<IConfigurationManager>(serviceProvider =>
                 new ConfigurationManager(
-                    builder ?? serviceProvider.GetService<IConfigurationBuilder>() ?? new ConfigurationBuilder(),
-                    args,
+                    builder ?? serviceProvider.GetService<IConfigurationBuilder>() ?? new ConfigurationBuilder(), args,
                     customizer));
+
+            return services;
+        }
     }
 }
