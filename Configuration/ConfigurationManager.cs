f509f306ae3d3375b8aece66771af746e55657a3
James Woodley <james@lightestnight.com>
2022-10-31 20:03:00 +0000

    Use IConfigurationRoot

--- Configuration/ConfigurationManager.cs
+++ Configuration/ConfigurationManager.cs
@@ -6,33 +6,40 @@ using Microsoft.Extensions.Primitives;
 
 namespace LightestNight.Configuration;
 
-public class ConfigurationManager : IConfiguration
+public class ConfigurationManager : IConfigurationRoot
 {
-    public IConfigurationRoot Configuration { get; }
-
+    private readonly IConfigurationRoot _root;
+    
     public ConfigurationManager(IConfigurationBuilder configurationBuilder, string[]? args = null,
         Func<IConfigurationBuilder, ConfigurationBuilder>? customizer = null)
     {
         if (configurationBuilder is not ConfigurationBuilder builder)
             throw new ArgumentNullException(nameof(configurationBuilder));
 
-        Configuration = ConfigureBuilder(builder, args, customizer);
+        _root = ConfigureBuilder(builder, args, customizer);
     }
 
     public IConfigurationSection GetSection(string key)
-        => Configuration.GetSection(key);
+        => _root.GetSection(key);
 
     public IEnumerable<IConfigurationSection> GetChildren()
-        => Configuration.GetChildren();
+        => _root.GetChildren();
 
     public IChangeToken GetReloadToken()
-        => Configuration.GetReloadToken();
+        => _root.GetReloadToken();
 
     public string this[string key]
     {
-        get => Configuration[key];
-        set => Configuration[key] = value;
+        get => _root[key];
+        set => _root[key] = value;
     }
+    
+    public void Reload()
+    {
+        _root.Reload();
+    }
+
+    public IEnumerable<IConfigurationProvider> Providers => _root.Providers;
 
     private static IConfigurationRoot ConfigureBuilder(IConfigurationBuilder builder, string[]? args = null,
         Func<IConfigurationBuilder, ConfigurationBuilder>? customizer = null)
@@ -77,84 +84,3 @@ public class ConfigurationManager : IConfiguration
         => Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT")
            ?? Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
 }
-/*public class ConfigurationManager : IConfigurationManager
-{
-    public IConfigurationRoot Configuration { get; }
-
-    public static ConfigurationManager GetConfigurationManager(string[]? args = null,
-        Func<IConfigurationBuilder, ConfigurationBuilder>? customizer = null)
-        => new(new ConfigurationBuilder(), args, customizer);
-
-    public ConfigurationManager(IConfigurationBuilder configurationBuilder, string[]? args = null,
-        Func<IConfigurationBuilder, ConfigurationBuilder>? customizer = null)
-    {
-        if (configurationBuilder is not ConfigurationBuilder builder)
-            throw new ArgumentNullException(nameof(configurationBuilder));
-
-        Configuration = ConfigureBuilder(builder, args, customizer);
-    }
-
-    public static IConfigurationRoot ConfigureBuilder(IConfigurationBuilder builder, string[]? args = null,
-        Func<IConfigurationBuilder, ConfigurationBuilder>? customizer = null)
-    {
-        builder.Sources.Clear();
-
-        builder.SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json", true, false);
-
-        var environment = GetEnvironmentFromCommandLine(args) ?? GetEnvironmentFromEnvironmentVariable();
-        if (!string.IsNullOrWhiteSpace(environment))
-            builder.AddJsonFile($"appsettings.{environment}.json", true, false);
-
-        builder.AddEnvironmentVariables();
-
-        if (args is not null)
-            builder.AddCommandLine(args);
-
-        if (customizer is not null)
-            builder = customizer(builder);
-
-        return builder.Build();
-    }
-
-    public TConfig Bind<TConfig>(string? sectionName = null) where TConfig : class, new()
-    {
-        var jsonSectionName = (sectionName ?? ConfigSectionNameAttribute.ReadFrom(typeof(TConfig)));
-        var bind = new TConfig();
-        
-        Configuration.GetSection(jsonSectionName).Bind(bind);
-        return bind;
-    }
-
-    public string GetConnectionString(string name)
-        => Configuration.GetConnectionString(name);
-
-    public TSetting? GetSetting<TSetting>(string name, TSetting? defaultValue = default)
-    {
-        var result = Configuration[name];
-
-        if (string.IsNullOrWhiteSpace(result))
-            return defaultValue;
-
-        return (TSetting)Convert.ChangeType(result, typeof(TSetting));
-    }
-
-    private static string? GetEnvironmentFromCommandLine(string[]? args)
-    {
-        if (args is null)
-            return null;
-        
-        var builder = new ConfigurationBuilder();
-        builder.AddCommandLine(args);
-        var commandLineParser = builder.Build();
-
-        return commandLineParser.GetValue<string?>("environment", null)
-               ?? commandLineParser.GetValue<string?>("Environment", null)
-               ?? commandLineParser.GetValue<string?>("env", null)
-               ?? commandLineParser.GetValue<string?>("Env", null)
-               ?? commandLineParser.GetValue<string?>("e", null);
-    }
-
-    private static string? GetEnvironmentFromEnvironmentVariable()
-        => Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")
-           ?? Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT");
-}*/
