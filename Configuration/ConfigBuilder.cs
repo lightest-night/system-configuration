bb899f49f3be7f1ade1b3eb8ec92f61ee83999c4
James Woodley <james@lightestnight.com>
2022-11-03 19:15:39 +0000

    Simplify down to minimal dotnet 6 standards

---
+++ Configuration/ConfigBuilder.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
+namespace LightestNight.Configuration;
+
+public static class ConfigBuilder
+{
+    public static void Build(HostBuilderContext hostingContext, IConfigurationBuilder config, string[]? args = null)
+    {
+        config.Sources.Clear();
+        var environment = hostingContext.HostingEnvironment.EnvironmentName;
+
+        config.SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", true, true)
+            .AddJsonFile($"appsettings.{environment}.json", true, true)
+            .AddEnvironmentVariables();
+
+        if (args is not null)
+            config.AddCommandLine(args);
+    }
+}
